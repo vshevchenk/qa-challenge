9e839c319c4ee33d540d64710302e237f0fe4dea
Valerii Shevchenko <vshevchenk@softserveinc.com>
2020-05-12 15:32:53 +0300

    Fix yaml unsafe loading
    Signed-off-by: Valerii Shevchenko <vshevchenk@softserveinc.com>

--- conftest.py
+++ conftest.py
@@ -7,5 +7,5 @@ from qa_challenge.model.credentials import Credentials
 @fixture
 def credentials():
     with open("credentials.yaml") as yaml_file:
-        credentials_yaml = yaml.load(yaml_file)
+        credentials_yaml = yaml.load(yaml_file, Loader=yaml.BaseLoader)
         return Credentials(credentials_yaml.get("email"), credentials_yaml.get("password"))
